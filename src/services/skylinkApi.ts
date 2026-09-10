17993c7781daf0e42d847d6e34f8fea4827817de
anyanwuifeanyio-rgb <anyanwuifeanyio@gmail.com>
2026-09-10 15:35:35 +0100

    Update skylinkApi.ts

--- src/services/skylinkApi.ts
+++ src/services/skylinkApi.ts
@@ -495,7 +495,7 @@ export class SkyLinkClient {
     const currency = (req.currency || 'NGN').toUpperCase();
 
     const searchPayload = {
-      search_mode: 'local',
+      search_mode: 'external',
       from: origin,
       to: destination,
       flight_type: (req.flight_type === 'oneway' || (req.flight_type as string) === 'one-way' || (req.flight_type as string) === 'one_way') ? 'oneway' : (req.flight_type === 'multicity' ? 'multicity' : 'roundtrip'),
